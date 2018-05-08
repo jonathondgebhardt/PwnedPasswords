206812517d19edec599d668006141ee0e0e5cdba
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2018-05-07 22:20:28 -0400

    Implemented insert
    Needs further testing and documentation.

--- Trie.cpp
+++ Trie.cpp
@@ -1,114 +1,126 @@
+#include "stdafx.h"
 #include "Trie.h"
 #include <string>
 #include <iostream>
 
-#define EMPTYNODE 'a'
+#define EMPTYNODE '!'
 #define HASHLENGTH 4
 
 Trie::Trie()
 {
-    root = Node(EMPTYNODE);
-    root.child = nullptr;
-    root.sibling = nullptr;
-    //root.value = EMPTYNODE;
+	root = Node();
+	/*rootchild = nullptr;
+	root.sibling = nullptr;*/
+	//root.value = EMPTYNODE;
 }
 
 Trie::~Trie()
 {
-    // std::cout << root->value << std::endl;
-    // Should iterate list destroying nodes
+	// std::cout << root->value << std::endl;
+	// Should iterate list destroying nodes
 }
 
 /*
-    Insert a new word into the trie. Duplicate words are not allowed. This function
-    returns true if the word is successfully inserted into the trie, and false if the
-    word could not be inserted.
+Insert a new word into the trie. Duplicate words are not allowed. This function
+returns true if the word is successfully inserted into the trie, and false if the
+word could not be inserted.
 */
 bool Trie::insert(std::string word)
 {
-    // Base case: sha1sum is a 40 char string
-    if (word.length() != HASHLENGTH)
-    {
-        return false;
-    }
+	// Base case: sha1sum is a 40 char string
+	if (word.length() != HASHLENGTH)
+	{
+		return false;
+	}
 
-    char currentLetter = (char) word[0];
-    std::cout << "Inserting " << word[0] << std::endl;
-    
-    // If trie is empty, we can insert first character
-    if (root.value == EMPTYNODE)
-    {
-        // Since root did not exist before, it's empty and we can insert
-        // value here
-        root.value = currentLetter;
-        root.child = root.child->insert(word.substr(1));
-        
-        return true;
-    }
+	char currentLetter = (char)word[0];
 
-    // Check if value is already in list, traverse if found
-    Node temp = root;
-    while (temp.sibling)
-    {
-        if (temp.value == currentLetter)
-        {
-            if (!temp.child)
-            {
-                temp.child = new Node(currentLetter);
-            }
-            
-            temp.child = temp.child->insert(word.substr(1));
-            return true;
-        }
+	// If trie is empty, we don't need to look for an appropriate
+	// node since there are none.
+	if (root.value == EMPTYNODE)
+	{
+		root = Node(currentLetter);
+		root.insert(word);
+		return true;
+	}
 
-        temp = *temp.sibling;
-    }
+	// Otherwise, we look for an appropriate node to insert on
+	else
+	{
+		Node* temp = &root;
+		while (temp)
+		{
+			// If the current node has the value we're searching for,
+			// we can stop.
+			if (temp->value == currentLetter)
+			{
+				break;
+			}
 
-    // Value is not in list, need to add
-    temp.sibling = new Node(currentLetter);
-    temp.sibling->child = new Node();
-    temp.sibling->child = temp.sibling->child->insert(word.substr(1));
+			// If the current node has a sibling, keep iterating.
+			if (temp->sibling)
+			{
+				temp = temp->sibling;
+			}
 
-    return true;
+			// If there's no more siblings and we haven't found the
+			// node we're looking for, we need to create a new sibling
+			// and insert on it.
+			else
+			{
+				temp->sibling = new Node(currentLetter);
+				temp->sibling->insert(word);
+				return true;
+			}
+
+		}
+
+		// We found the node we're looking for. If it doesn't have a child,
+		// create one. In both cases, insert on the child.
+		if (!temp->child)
+		{
+			temp->child = new Node();
+		}
+
+		temp->child->insert(word.substr(1));
+		return true;
+	}
+
+	return false;
 }
 
 /*
-    Return the number of words in the trie
+Return the number of words in the trie
 */
 int Trie::count()
 {
-    return -1;
+	return -1;
 }
 
 /*
-    Return the total number of nodes in the trie
+Return the total number of nodes in the trie
 */
 int Trie::getSize()
 {
-    return -1;
+	return -1;
 }
 
 /*
-    If the given word is in the trie, return true, otherwise return false
+If the given word is in the trie, return true, otherwise return false
 */
 bool Trie::find(std::string prefix)
 {
-    return false;
+	return false;
 }
 
 /*
-    Print out contents of trie using depth first traversal
+Print out contents of trie using depth first traversal
 */
 void Trie::display()
 {
-    // Print root, this is the first node
-    std::cout << "---Trie contents---\n" << root.value << std::endl;
+	// Print root, this is the first node
+	std::cout << "---Trie contents---\n" << std::endl;
 
-    Node* temp = root.sibling;
-    while (temp) 
-    {
-        std::cout << temp->value << std::endl;
-        temp = temp->sibling;
-    }
+	root.display();
 
 }
--- Trie.h
+++ Trie.h
@@ -1,111 +1,154 @@
 #ifndef TRIE_H
 #define TRIE_H
 
-#define EMPTYNODE 'a'
+#define EMPTYNODE '!'
 
+#include "stdafx.h"
 #include <string>
 #include <iostream>
 
 class Trie
 {
-    private:
+private:
 
-        class Node
-        {
-            public:
-                Node* child;
-                Node* sibling;
-                char value;
+	class Node
+	{
+	public:
+		Node* child;
+		Node* sibling;
+		char value;
 
-                Node(char value) : value(value)
-                {
-                    child = nullptr;
-                    sibling = nullptr;
-                }
+		Node(char value) : value(value)
+		{
+			child = nullptr;
+			sibling = nullptr;
+		}
 
-                Node()
-                {
-                    child = nullptr;
-                    sibling = nullptr;
-                    value = EMPTYNODE;
-                }
+		Node()
+		{
+			child = nullptr;
+			sibling = nullptr;
+			value = EMPTYNODE;
+		}
 
-                Node* insert(std::string word)
-                {
-                    if (word.length() < 1)
-                    {
-                        return this;
-                    }
+		void insert(std::string word)
+		{
+			// Base case: If there's no more letters to add
+			if (word.length() < 1)
+			{
+				return;
+			}
 
-                    bool found = false;
-                    char currentLetter = (char) word[0];
+			char currentLetter = (char)word[0];
 
-                    std::cout << "\tIn " << value << " insert" << std::endl;
-                    
-                    // Check if value is already in list, traverse if found
-                    Node* temp = this;
-                    while (temp->sibling)
-                    {
-                        if (temp->value == currentLetter)
-                        {
-                            if (temp->child)
-                            {
-                                temp->child = new Node(currentLetter);
-                            }
-                            
-                            temp->child = temp->child->insert(word.substr(1));
-                            found = true;
-                            break;
-                        }
+			// If the node we're on is empty, we can insert. Initialize
+			// value and insert substring.
+			if (this->value == EMPTYNODE)
+			{
+				this->value = currentLetter;
 
-                        temp = temp->sibling;
-                    }
+				if (word.substr(1).length() >= 1)
+				{
+					this->child = new Node();
+					this->child->insert(word.substr(1));
+				}
+			}
 
-                    // Value is not in list, need to add
-                    if (!found)
-                    {
-                        std::cout << "In not found statement" << std::endl;
-                        temp->sibling = new Node(currentLetter);
-                        temp->sibling->child = new Node();
-                        temp->sibling->child = temp->sibling->child->insert(word.substr(1));
-                    }
-                    
-                }
+			// Otherwise, we need to find an appropriate node
+			else
+			{
+				Node* temp = this;
+				while (temp)
+				{
+					// If the node has the value we are looking for, we can stop
+					if (temp->value == currentLetter)
+					{
+						break;
+					}
 
-                // Checks breadth for target node
-                Node* nodeExists(char target)
-                {
-                    if (!this)
-                    {
-                        return nullptr;
-                    }
+					// Iterate if there are more siblings to look at
+					if (temp->sibling)
+					{
+						temp = temp->sibling;
+					}
 
-                    std::cout << "\t\t" << value << " ?= " << target << std::endl;
+					// If there are no more siblings, and we haven't found the value
+					// we're lookng for, we need a new sibling. Create one and insert
+					// on that.
+					else
+					{
+						temp->sibling = new Node();
+						temp->sibling->insert(word);
+						return;
+					}
+					
+				}
 
-                    if (value == target)
-                    {
-                        return this;
-                    }
+				// We found the node we're looking for and must create a new
+				// child.
+				if (word.substr(1).length() >= 1)
+				{
+					if (!temp->child)
+					{
+						temp->child = new Node();
+					}
 
-                    return sibling->nodeExists(target);
-                }
-            
-        };
+					temp->child->insert(word.substr(1));
+				}
+				
+			}
+			
+		}
 
-        Node root;
+		// Checks breadth for target node
+		Node* nodeExists(char target)
+		{
+			// Base case: if called on empty node, nothing is in the layer yet.
+			// Return a new node.
+			if (this->value = EMPTYNODE)
+			{
+				return new Node(target);
+			}
 
-        
-        
+			// Found node we were looking for, return this.
+			if (value == target)
+			{
+				return this;
+			}
 
-    public:
-        Trie();
-        ~Trie();
-        bool insert(std::string word);
-        int count();
-        int getSize();
-        bool find(std::string prefix);
-        void display();
-        // friend ostream& operator<<(ostream& os, const MagicBag& mb);
+			// This isn't the node we're looking for and other nodes exist.
+			// Keep looking.
+			return sibling->nodeExists(target);
+		}
+
+		void display()
+		{
+			// Print root, this is the first node
+			std::cout << this->value << std::endl;
+
+			Node* temp = this;
+			while (temp)
+			{
+				std::cout << temp->value << std::endl;
+				temp = temp->sibling;
+			}
+
+		}
+
+	};
+
+	Node root;
+
+
+public:
+	Trie();
+	~Trie();
+	bool insert(std::string word);
+	int count();
+	int getSize();
+	bool find(std::string prefix);
+	void display();
+	// friend ostream& operator<<(ostream& os, const MagicBag& mb);
 
 };
 
